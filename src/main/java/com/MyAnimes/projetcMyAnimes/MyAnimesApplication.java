379bb069f1e35f9563596c12a1ae424bfb3c35d6
Yan Victor <137448055+YanVictor00@users.noreply.github.com>
2025-08-14 20:46:26 -0300

    initial commit

---
+++ src/main/java/com/MyAnimes/projetcMyAnimes/MyAnimesApplication.java
@@ -0,0 +1,13 @@
+package com.MyAnimes.projetcMyAnimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAnimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAnimesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MyAnimes/projetcMyAnimes/MyAnimesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyAnimes.projetcMyAnimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAnimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
